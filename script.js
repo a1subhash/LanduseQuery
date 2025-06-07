745af6e8199fea938de914381b395b54f3a2b09e
a1subhash <goswamisubhash166@gmail.com>
2025-06-07 18:25:41 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const csvUrl = "https://raw.githubusercontent.com/a1subhash/LanduseQuery/refs/heads/main/dewahi.csv"";
+const csvUrl = "https://raw.githubusercontent.com/a1subhash/LanduseQuery/refs/heads/main/dewahi.csv";
 let csvData = [];
 
 // Fetch CSV data on page load
